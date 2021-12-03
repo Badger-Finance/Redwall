c9cae063554667119971580704e353f402051b32
Elio <eliobricenov@gmail.com>
2021-12-03 18:45:05 -0400

    chore: run linter

--- src/pages/attacker/index.tsx
+++ src/pages/attacker/index.tsx
@@ -17,7 +17,9 @@ function AttackerInfo(): JSX.Element {
   const [userApprovals, setUserApprovals] = useState<
     Record<string, ApprovalFragment[]>
   >({});
-  const [vaultApprovals, setVaultApprovals] = useState<Record<string, number>>({});
+  const [vaultApprovals, setVaultApprovals] = useState<Record<string, number>>(
+    {},
+  );
   const [vaultAmounts, setVaultAmounts] = useState<Record<string, number>>({});
   const [userAmounts, setUserAmounts] = useState<Record<string, number>>({});
 
@@ -49,9 +51,13 @@ function AttackerInfo(): JSX.Element {
         console.log(vaultApprovals);
         setVaultApprovals(vaultApprovals);
 
-        const userAccounts: Record<string, Account> = Object.fromEntries(await Promise.all(Object.keys(userApprovals).map(async (acc) => {
-          return [acc, await api.loadAccount(acc)];
-        })));
+        const userAccounts: Record<string, Account> = Object.fromEntries(
+          await Promise.all(
+            Object.keys(userApprovals).map(async (acc) => {
+              return [acc, await api.loadAccount(acc)];
+            }),
+          ),
+        );
 
         const vaultTotals: Record<string, number> = {};
         const userTotals: Record<string, number> = {};
@@ -111,12 +117,20 @@ function AttackerInfo(): JSX.Element {
                   key={token.id}
                   className="flex flex-col bg-gray-100 p-4 m-1 text-black my-3 shadow-lg"
                 >
-                  <span className="text-xl mb-1 text-haze font-bold leading-tight cursor-pointer" onClick={() => window.open(`https://etherscan.io/address/${token.id}`)}>{token.name}</span>
-                    <span className="text-xl font-bold">
-                      {formatter.format(vaultAmounts[token.id] ?? 0)} at risk
-                    </span>
+                  <span
+                    className="text-xl mb-1 text-haze font-bold leading-tight cursor-pointer"
+                    onClick={() =>
+                      window.open(`https://etherscan.io/address/${token.id}`)
+                    }
+                  >
+                    {token.name}
+                  </span>
+                  <span className="text-xl font-bold">
+                    {formatter.format(vaultAmounts[token.id] ?? 0)} at risk
+                  </span>
                   <span className="text-lg">
-                    {vaultApprovals[ethers.utils.getAddress(token.id)] ?? 0} outstanding approvals
+                    {vaultApprovals[ethers.utils.getAddress(token.id)] ?? 0}{' '}
+                    outstanding approvals
                   </span>
                   <div className="flex space-x-3 leading-tighte text-sm text-gray">
                     <div className="flex">{approvals} total approvals</div>
@@ -129,53 +143,71 @@ function AttackerInfo(): JSX.Element {
           <div className="mt-4 text-md text-black bg-raspberry p-4 mb-2">
             At Risk Users
           </div>
-          {Object.keys(userApprovals).sort((a, b) => {
-            const userA = userAmounts[a] ?? 0;
-            const userB = userAmounts[b] ?? 0;
-            return userB - userA;
-          }).map((user) => {
-            const approvals = userApprovals[user];
-            const atRisk = userAmounts[user] ?? 0;
-            const compromised = atRisk <= 0;
-            return (
-              <div
-                key={user}
-                className={"flex flex-col p-2 m-2 text-black" + (compromised ? ' bg-red-200' : ' bg-skull')}
-              >
-                <div className="flex w-full justify-between items-center">
-                  <span className="text-lg font-bold p-2 cursor-pointer text-haze" onClick={() => window.open(`https://etherscan.io/address/${user}`)}>{`${user} • ${formatter.format(atRisk)} at risk${compromised ? ' • [COMPROMISED]' : ''}`}</span>
-                  <span className="font-semibold mr-4">
-                    {approvals.length} approvals
-                  </span>
-                </div>
-                {approvals.map((approval) => {
-                  const { amount, id, hash, owner, timestamp, token } = approval;
-                  const approvalAmount = BigNumber.from(amount).gte(0)
-                    ? 'Max'
-                    : ethers.utils.formatEther(amount);
-                  return (
-                    <div key={id} className="flex p-2 m-2 text-black">
-                      <div className="flex flex-col flex-grow cursor-pointer">
-                        <span className="font-semibold text-sm text-raspberry" onClick={() => window.open(`https://etherscan.io/tx/${hash}`)}>
-                          {hash}
+          {Object.keys(userApprovals)
+            .sort((a, b) => {
+              const userA = userAmounts[a] ?? 0;
+              const userB = userAmounts[b] ?? 0;
+              return userB - userA;
+            })
+            .map((user) => {
+              const approvals = userApprovals[user];
+              const atRisk = userAmounts[user] ?? 0;
+              const compromised = atRisk <= 0;
+              return (
+                <div
+                  key={user}
+                  className={
+                    'flex flex-col p-2 m-2 text-black' +
+                    (compromised ? ' bg-red-200' : ' bg-skull')
+                  }
+                >
+                  <div className="flex w-full justify-between items-center">
+                    <span
+                      className="text-lg font-bold p-2 cursor-pointer text-haze"
+                      onClick={() =>
+                        window.open(`https://etherscan.io/address/${user}`)
+                      }
+                    >{`${user} • ${formatter.format(atRisk)} at risk${
+                      compromised ? ' • [COMPROMISED]' : ''
+                    }`}</span>
+                    <span className="font-semibold mr-4">
+                      {approvals.length} approvals
+                    </span>
+                  </div>
+                  {approvals.map((approval) => {
+                    const { amount, id, hash, owner, timestamp, token } =
+                      approval;
+                    const approvalAmount = BigNumber.from(amount).gte(0)
+                      ? 'Max'
+                      : ethers.utils.formatEther(amount);
+                    return (
+                      <div key={id} className="flex p-2 m-2 text-black">
+                        <div className="flex flex-col flex-grow cursor-pointer">
+                          <span
+                            className="font-semibold text-sm text-raspberry"
+                            onClick={() =>
+                              window.open(`https://etherscan.io/tx/${hash}`)
+                            }
+                          >
+                            {hash}
+                          </span>
+                          <span className="font-semibold text-xs text-cave">
+                            Approved at{' '}
+                            {new Date(timestamp * 1000).toLocaleString()}
+                          </span>
+                        </div>
+                        <span className="flex items-center mx-4">
+                          {token.name}
                         </span>
-                        <span className="font-semibold text-xs text-cave">
-                          Approved at{' '}
-                          {new Date(timestamp * 1000).toLocaleString()}
+                        <span className="flex items-center mx-4">
+                          {approvalAmount}
                         </span>
                       </div>
-                      <span className="flex items-center mx-4">
-                        {token.name}
-                      </span>
-                      <span className="flex items-center mx-4">
-                        {approvalAmount}
-                      </span>
-                    </div>
-                  );
-                })}
-              </div>
-            );
-          })}
+                    );
+                  })}
+                </div>
+              );
+            })}
         </div>
       )}
     </div>
