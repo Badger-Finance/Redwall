1fec59c86f06979d4402189095e161e14b846591
jintao <hellojintao@protonmail.com>
2021-12-20 16:30:29 +0000

    chore: add subgraph files

---
+++ subgraph/src/entities/approval-day-data.ts
@@ -0,0 +1,24 @@
+import { Address } from '@graphprotocol/graph-ts';
+import { ApprovalDayData } from '../../generated/schema';
+import { ZERO } from '../constants';
+import { loadToken } from './token';
+
+export function loadApprovalDayData(day: i32, token: Address): ApprovalDayData {
+  let id = 'ADD-'
+    .concat(day.toString())
+    .concat('-')
+    .concat(token.toHexString());
+  let approval = ApprovalDayData.load(id) as ApprovalDayData;
+
+  if (approval) {
+    return approval;
+  }
+
+  approval = new ApprovalDayData(id);
+  approval.timestamp = day;
+  approval.token = loadToken(token).id;
+  approval.approvals = ZERO;
+  approval.revokes = ZERO;
+  approval.save();
+  return approval;
+}
---
+++ subgraph/src/entities/contracts.ts
@@ -0,0 +1,11 @@
+import { ethereum } from '@graphprotocol/graph-ts';
+
+export function readValue<T>(
+  callResult: ethereum.CallResult<T>,
+  defaultValue: T,
+): T {
+  if (callResult.reverted) {
+    return defaultValue;
+  }
+  return callResult.value;
+}
