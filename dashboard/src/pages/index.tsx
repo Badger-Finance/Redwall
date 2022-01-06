b0650fbbffcfc8ab8f21e34e05443bdbe0a7be46
jintao <hellojintao@protonmail.com>
2022-01-06 12:59:20 -0500

    chore: update workspaces format

--- dashboard/src/pages/index.tsx
+++ dashboard/src/pages/index.tsx
@@ -1,5 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { CumulativeApprovalFragment, CumulativeApproval_OrderBy, OrderDirection, UserApprovalDayDataFragment, UserApprovalDayData_OrderBy } from '../graphql/generated/badger';
+import {
+  CumulativeApprovalFragment,
+  CumulativeApproval_OrderBy,
+  OrderDirection,
+  UserApprovalDayDataFragment,
+  UserApprovalDayData_OrderBy,
+} from '../graphql/generated/badger';
 import { SdkContext } from '../sdk-context';
 
 export enum Timeframe {
@@ -10,10 +16,14 @@ export enum Timeframe {
 export default function Home(): JSX.Element {
   const sdk = useContext(SdkContext);
 
-  const day = Number(((Date.now() / 1000) / (60 * 60 * 24)).toFixed()) - 1;
+  const day = Number((Date.now() / 1000 / (60 * 60 * 24)).toFixed()) - 1;
   const [timeframe, setTimeFrame] = useState(Timeframe.Today);
-  const [totalApprovals, setTotalApprovals] = useState<CumulativeApprovalFragment[]>([]);
-  const [todayApprovals, setTodayApprovals] = useState<UserApprovalDayDataFragment[]>([]);
+  const [totalApprovals, setTotalApprovals] = useState<
+    CumulativeApprovalFragment[]
+  >([]);
+  const [todayApprovals, setTodayApprovals] = useState<
+    UserApprovalDayDataFragment[]
+  >([]);
 
   useEffect(() => {
     async function loadApprovalsLeaderboard() {
@@ -40,62 +50,82 @@ export default function Home(): JSX.Element {
           setTodayApprovals(todayApprovals.userApprovalDayDatas);
         }
       }
-      }
+    }
     loadApprovalsLeaderboard();
   }, [timeframe]);
 
   return (
-  <div className="flex flex-grow flex-col items-center justify-center my-6">
-  <div className="flex flex-col w-3/4">
-  {timeframe === Timeframe.Today && (
-    <>
-  <div className="text-xl">Todays Token Approvals</div>
-  {todayApprovals.length > 0 && 
-    <div className="w-full mt-4">
-      <div className="flex items-center justify-center border-b mb-2 pb-1">
-        <div className="w-2/6">Spender</div>
-        <div className="w-2/6">Token</div>
-        <div className="w-1/6">Approvals</div>
-        <div className="w-1/6">Revokes</div>
+    <div className="flex flex-grow flex-col items-center justify-center my-6">
+      <div className="flex flex-col w-3/4">
+        {timeframe === Timeframe.Today && (
+          <>
+            <div className="text-xl">Todays Token Approvals</div>
+            {todayApprovals.length > 0 && (
+              <div className="w-full mt-4">
+                <div className="flex items-center justify-center border-b mb-2 pb-1">
+                  <div className="w-2/6">Spender</div>
+                  <div className="w-2/6">Token</div>
+                  <div className="w-1/6">Approvals</div>
+                  <div className="w-1/6">Revokes</div>
+                </div>
+                {todayApprovals.map((approval) => {
+                  return (
+                    <div key={approval.id} className="flex w-full my-1">
+                      <div
+                        className="w-2/6 cursor-pointer text-green-200"
+                        onClick={() =>
+                          window.open(
+                            `https://etherscan.io/address/${approval.spender.id}`,
+                          )
+                        }
+                      >
+                        {approval.spender.id}
+                      </div>
+                      <div className="w-2/6">{approval.token.name}</div>
+                      <div className="w-1/6">{approval.approvals}</div>
+                      <div className="w-1/6">{approval.revokes}</div>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </>
+        )}
+        {timeframe === Timeframe.Lifetime && (
+          <>
+            <div className="text-xl">Total Token Approvals</div>
+            {totalApprovals.length > 0 && (
+              <div className="w-full mt-4">
+                <div className="flex items-center justify-center border-b mb-2 pb-1">
+                  <div className="w-2/6">Spender</div>
+                  <div className="w-2/6">Token</div>
+                  <div className="w-1/6">Approvals</div>
+                  <div className="w-1/6">Revokes</div>
+                </div>
+                {totalApprovals.slice(0, 25).map((approval) => {
+                  return (
+                    <div key={approval.id} className="flex w-full my-1">
+                      <div
+                        className="w-2/6 cursor-pointer text-green-200"
+                        onClick={() =>
+                          window.open(
+                            `https://etherscan.io/address/${approval.spender.id}`,
+                          )
+                        }
+                      >
+                        {approval.spender.id}
+                      </div>
+                      <div className="w-2/6">{approval.token.name}</div>
+                      <div className="w-1/6">{approval.approvals}</div>
+                      <div className="w-1/6">{approval.revokes}</div>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </>
+        )}
       </div>
-      {todayApprovals.map((approval) => {
-        return (
-          <div key={approval.id} className="flex w-full my-1">
-            <div className="w-2/6 cursor-pointer text-green-200" onClick={() => window.open(`https://etherscan.io/address/${approval.spender.id}`)}>{approval.spender.id}</div>
-            <div className="w-2/6">{approval.token.name}</div>
-            <div className="w-1/6">{approval.approvals}</div>
-            <div className="w-1/6">{approval.revokes}</div>
-          </div>
-        )
-      })}
-      </div>
-      }
-    </>
-  )}
-  {timeframe === Timeframe.Lifetime && (
-    <><div className="text-xl">Total Token Approvals</div>
-    {totalApprovals.length > 0 && 
-      <div className="w-full mt-4">
-        <div className="flex items-center justify-center border-b mb-2 pb-1">
-          <div className="w-2/6">Spender</div>
-          <div className="w-2/6">Token</div>
-          <div className="w-1/6">Approvals</div>
-          <div className="w-1/6">Revokes</div>
-        </div>
-        {totalApprovals.slice(0, 25).map((approval) => {
-          return (
-            <div key={approval.id} className="flex w-full my-1">
-              <div className="w-2/6 cursor-pointer text-green-200" onClick={() => window.open(`https://etherscan.io/address/${approval.spender.id}`)}>{approval.spender.id}</div>
-              <div className="w-2/6">{approval.token.name}</div>
-              <div className="w-1/6">{approval.approvals}</div>
-              <div className="w-1/6">{approval.revokes}</div>
-            </div>
-          )
-        })}
-      </div>
-    }
-    </>
-  )}
-  </div>
-  </div>);
+    </div>
+  );
 }
